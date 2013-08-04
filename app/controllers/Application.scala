c2c1dc5317bfa6b722e0cf7c7a6ea17a2518e3bd
Marius Ropotica <marius.ropotica@basware.com>
2013-08-04 15:39:49 +0300

    A

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,12 +1,31 @@
 package controllers
 
-import play.api._
-import play.api.mvc._
+import play.api.libs.iteratee.Enumerator
+import play.api.mvc.Action
+import play.api.mvc.Controller
+import play.api.mvc.Cookie
+import play.api.mvc.ResponseHeader
+import play.api.mvc.SimpleResult
 
 object Application extends Controller {
-  
+
   def index = Action {
-    Ok(views.html.index("Your new application is ready."))
+    Ok(views.html.index("Index page")).withCookies(Cookie("a", "b"))
   }
-  
+
+  def request1 = Action {
+    SimpleResult(
+      header = ResponseHeader(200, Map(CONTENT_TYPE -> "text/plain")),
+      body = Enumerator("text/plain request"))
+  }
+
+  def request2 = Action {
+    SimpleResult(
+      header = ResponseHeader(200, Map(CONTENT_TYPE -> "application/json")),
+      body = Enumerator("{foo: 1, bar: 2}"))
+  }
+
+  def request3 = Action {
+    Ok(views.html.request3("A content"))
+  }
 }
